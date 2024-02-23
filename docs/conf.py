b660caa3065c8bd799c923b25f2823a3ade5bc85
RFOxbury <rosamundoxbury@gmail.com>
2024-02-23 09:33:43 +0000

    Setup Nesta Data Science cookiecutter

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+"""Sphinx configuration."""
+
+from datetime import datetime
+
+
+project = "dsp_ai_eval"
+author = "Nesta"
+copyright = f"{datetime.now().year}, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    # "sphinx_click",
+    "sphinx_rtd_theme",
+]
+autodoc_typehints = "description"
+html_theme = "sphinx_rtd_theme"
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/dsp_ai_eval
+   License <license>
+
+dsp_ai_eval
+===============================
+
+A rapid exploration of evaluating outputs from generative AI tools
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
