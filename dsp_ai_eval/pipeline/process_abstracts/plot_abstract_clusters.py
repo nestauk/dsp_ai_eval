677b0e0571b14299b63ac332e391c607ea2fcb47
RFOxbury <rosamundoxbury@gmail.com>
2024-05-13 09:29:52 +0100

    Add docstrings

--- dsp_ai_eval/pipeline/process_abstracts/plot_abstract_clusters.py
+++ dsp_ai_eval/pipeline/process_abstracts/plot_abstract_clusters.py
@@ -26,37 +26,107 @@ embedding_model = SentenceTransformer(config["embedding_model"])
 SEED = config["seed"]
 
 
-def create_chart(df_vis, scale_by_citations=False, save=True, filename_suffix=""):
-    # Conditional field calculation based on `scale_by_citations` flag
-    if scale_by_citations:
-        size_encode = alt.Size(
-            "size_calculated:Q",
-            scale=alt.Scale(range=[0, 2000]),
-            legend=alt.Legend(title="N citations"),
-        )
+def map_citations_to_size(citations: int, quantile_values: dict) -> str:
+    """
+    Maps the number of citations to a size category based on quantile thresholds.
+    The string output can be used to set point sizes in an Altair chart.
 
-        tooltip_fields = ["topic_name:N", "doc:N", "total_cites:N"]
+    Parameters:
+        citations (int): The number of citations.
+        quantile_values (dict): A dictionary with quantiles as keys and the corresponding threshold values as values.
+
+    Returns:
+        str: A string indicating the citation size category.
+    """
+
+    a = int(quantile_values[0.25])
+    b = int(quantile_values[0.5])
+    c = int(quantile_values[0.75])
+
+    if citations < a:
+        return f"5-{a-1}"
+    elif a <= citations < b:
+        return f"{a}-{b-1}"
+    elif b <= citations < c:
+        return f"{b}-{c-1}"
     else:
-        size_encode = alt.value(50)
+        return f"{c}+"
 
-        tooltip_fields = ["topic_name:N", "doc:N"]
 
-    opacity_condition = alt.condition(
-        alt.datum.topic_name == "NA", alt.value(0.2), alt.value(0.5)
+def create_chart(
+    df_vis: pd.DataFrame,
+    scale_by_citations: bool = False,
+    save: bool = True,
+    filename_suffix: str = "",
+) -> None:
+    """
+    Generates and optionally saves a visualization chart based on citation data.
+
+    Parameters:
+        df_vis (pd.DataFrame): A DataFrame containing visualization data, must include a column 'total_cites'.
+        scale_by_citations (bool, optional): A flag to determine if the size of the points in the chart should be scaled
+            based on the number of citations. Defaults to False.
+        save (bool, optional): A flag to determine if the chart should be saved to a file. Defaults to True.
+        filename_suffix (str, optional): A suffix to append to the filename when saving the chart. Defaults to an empty string.
+
+    Returns:
+        None: The function generates and displays a chart, and optionally saves it.
+    """
+    # Create quantile bins for scaling the point size in the chart
+    quantile_values = df_vis["total_cites"].quantile([0.25, 0.5, 0.75])
+    logging.info(f"quantiles: {quantile_values}")
+
+    df_vis["point_size"] = df_vis["total_cites"].apply(
+        lambda x: map_citations_to_size(x, quantile_values)
     )
 
-    # Base chart setup
+    logging.info(df_vis["point_size"].value_counts(dropna=False))
+
+    # Conditional field calculation based on `scale_by_citations` flag
     if scale_by_citations:
-        chart = alt.Chart(df_vis).transform_calculate(
-            # Create a new field for size, multiplying 'total_cites' by 10
-            size_calculated="datum.total_cites * 10"  #'log(datum.total_cites + 1)' # Tried log-scaling but it didn't look great
+
+        a = int(quantile_values[0.25])
+        b = int(quantile_values[0.5])
+        c = int(quantile_values[0.75])
+
+        # Create a calculated field for binning citation counts into categories
+        chart = alt.Chart(df_vis)
+        size_encode = alt.Size(
+            "point_size:O",  # 'O' for ordinal since we're using discrete categories
+            scale=alt.Scale(
+                domain=[
+                    f"5-{a-1}",
+                    f"{a}-{b-1}",
+                    f"{b}-{c-1}",
+                    f"{c}+",
+                ],  # These should match the outputs from map_citations_to_size
+                range=[50, 200, 500, 1000],
+            ),
+            legend=alt.Legend(
+                title="Number of citations",
+                titleFontSize=12,
+                labelPadding=100,
+                labelFontSize=12,
+            ),
         )
+        tooltip_fields = ["topic_name:N", "doc:N", "total_cites:N", "point_size:O"]
     else:
         chart = alt.Chart(df_vis)
+        size_encode = alt.value(50)  # Use a constant size when not scaling by citations
+        tooltip_fields = ["topic_name:N", "doc:N"]
+
+    # Make the noise cluster more transparent than the other clusters
+    opacity_condition = alt.condition(
+        alt.datum.topic_name == "NA", alt.value(0.25), alt.value(0.6)
+    )
 
     base = chart.encode(
-        x=alt.X("x:Q", axis=alt.Axis(ticks=False, labels=False, title=None)),
-        y=alt.Y("y:Q", axis=alt.Axis(ticks=False, labels=False, title=None)),
+        x=alt.X(
+            "x:Q", axis=alt.Axis(ticks=False, labels=False, title=None, grid=False)
+        ),
+        y=alt.Y(
+            "y:Q", axis=alt.Axis(ticks=False, labels=False, title=None, grid=False)
+        ),
         size=size_encode,
         color=alt.Color("topic_name:N", legend=None).scale(
             scheme=config["abstracts_pipeline"]["cluster_colours"]
@@ -67,7 +137,7 @@ def create_chart(df_vis, scale_by_citations=False, save=True, filename_suffix=""
 
     plot = (base).properties(width=800, height=600).interactive()
 
-    # Saving logic, customized based on parameters
+    # Save the chart
     if save:
         filename = f"scite_abstracts{filename_suffix}.html"
         plot.save(PROJECT_DIR / f"outputs/figures/{filename}")
